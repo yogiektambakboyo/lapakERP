4d3522fd3e859d973c12c117998cc038775a17da
Yogi Aditya <it.yogi@borwita.co.id>
2023-01-11 22:15:48 +0700

    add trip report

--- resources/views/pages/reports/sales_trip.blade.php
+++ resources/views/pages/reports/sales_trip.blade.php
@@ -89,7 +89,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form action="{{ route('reports.cashier.search') }}" method="GET">   
+                    <form action="{{ route('reports.sales_trip.search') }}" method="GET">   
                         @csrf 
                         <div class="col-md-10">
                             <label class="form-label col-form-label col-md-4">@lang('general.lbl_branch')</label>
