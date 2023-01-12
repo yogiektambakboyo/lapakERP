103162887ce344c6167294c75664991ffc656cb4
Yogi Aditya <it.yogi@borwita.co.id>
2023-01-12 20:01:16 +0700

    trip detail

--- resources/views/pages/reports/sales_trip_detail.blade.php
+++ resources/views/pages/reports/sales_trip_detail.blade.php
@@ -85,7 +85,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form action="{{ route('reports.sales_trip.search') }}" method="GET">   
+                    <form action="{{ route('reports.sales_trip_detail.search') }}" method="GET">   
                         @csrf 
                         <div class="col-md-10">
                             <label class="form-label col-form-label col-md-4">@lang('general.lbl_branch')</label>
