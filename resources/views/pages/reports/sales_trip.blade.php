f9c5b9580bb2bcb2b8b731f7c126011ebbbbb48a
Yogi Aditya <it.yogi@borwita.co.id>
2023-01-13 00:40:21 +0700

    image

--- resources/views/pages/reports/sales_trip.blade.php
+++ resources/views/pages/reports/sales_trip.blade.php
@@ -190,7 +190,7 @@
         </div>
 
         <div class="modal fade" id="modal-filterimg" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog">
+            <div class="modal-dialog  modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
                 <h5 class="modal-title"  id="input_expired_list_at_lbl">Gambar</h5>
