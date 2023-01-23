2aca26814fe9ed29b44305f66a3ff1808c9e59cf
Yogi Aditya <it.yogi@borwita.co.id>
2023-01-23 21:04:41 +0700

    ignore

--- resources/views/pages/invoices/index.blade.php
+++ resources/views/pages/invoices/index.blade.php
@@ -55,7 +55,7 @@
                         <td>{{ $order->dated }}</td>
                         <td>{{ $order->customer }}</td>
                         <td>{{ number_format($order->total,0,',','.') }}</td>
-                        <td>{{ number_format($order->total_discount,0,',','.') }}</td>
+                        <td>{{ number_format($order->total_payment,0,',','.') }}</td>
                         <td  @if ($order->total_payment < $order->total) class="bg-warning" @endif>{{ number_format($order->total_payment,0,',','.') }}</td>
                         <td><a href="{{ route('invoices.show', $order->id) }}" class="btn btn-warning btn-sm  {{ $act_permission->allow_show==1?'':'d-none' }}">@lang('general.lbl_show')</a></td>
                         @if (($order->is_checkout == 0)&&($order->total_payment >= $order->total))
