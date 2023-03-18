c5296ebfc0c6ca83dcf78c9ddf7459b9ad34365a
Yogi Aditya <it.yogi@borwita.co.id>
2023-03-18 14:07:19 +0700

    total

--- resources/views/pages/reports/daily_print.blade.php
+++ resources/views/pages/reports/daily_print.blade.php
@@ -435,14 +435,12 @@
                           @endif
                       @endforeach
                     </td>
-                    <td>
-                      @foreach($out_datas as $out_data) 
-                          @php
-                            if($detail->id == $out_data->id ){
-                                  echo $out_data->abbr."/".$out_data->qty."<br>";
-                                  $c_pn=$c_pn+$out_data->qty;
-                            }            
-                          @endphp
+                    <td style="text-align: left;">
+                      @foreach($dtt_item_only as $diox)
+                          @if($diox->type_id==1 && $diox->customers_id == $detail->id && $diox->refbuy == 0)
+                                {{ $diox->product_name }} / {{ $diox->qty }}<br>
+                                @php $c_pn=$c_pn+$diox->qty; @endphp
+                          @endif
                       @endforeach
                     </td>
                 </tr>
@@ -577,7 +575,7 @@
                     <th></th>
                     <th>{{ number_format($c_p,0,',','.') }}</th>
                     <th>{{ number_format($t_p,0,',','.') }}</th>
-                    <th>
+                    <th style="text-align: left;">
                       @foreach($out_datas_total as $out_data_total) 
                           @php
                               echo $out_data_total->abbr."/".$out_data_total->qty."<br>";
