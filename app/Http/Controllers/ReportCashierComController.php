3c873ed28447e973e181a8208745ed5ecaae6ab4
Yogi Aditya <it.yogi@borwita.co.id>
2023-03-21 22:33:58 +0700

    add qty

--- app/Http/Controllers/ReportCashierComController.php
+++ app/Http/Controllers/ReportCashierComController.php
@@ -227,6 +227,21 @@ class ReportCashierComController extends Controller
                         join product_commisions pc on pc.product_id = id.product_id and pc.branch_id = c.branch_id
                         join users u on u.id = im.created_by and u.job_id = 1  and u.id = im.created_by 
                         where pc.created_by_fee > 0 and im.dated between '".$begindate."' and '".$enddate."' 
+                        union
+                        select u.id,'extra_commision' as com_type,im.dated,im.invoice_no,ps.abbr,ps.remark,im.created_by,u.name,id.price,id.qty,id.total,pc.values as base_commision ,sum(pc.values*id.qty) as commisions
+                        from invoice_master im 
+                        join invoice_detail id on id.invoice_no = im.invoice_no
+                        join product_sku ps on ps.id = id.product_id 
+                        join customers c on c.id = im.customers_id 
+                        join branch b on b.id = c.branch_id
+                        join product_commision_by_year pc on pc.product_id = id.product_id and pc.branch_id = c.branch_id
+                        join (
+                            select r.id,r.name,r.job_id,case when date_part('year', age(now(),join_date))::int=0 then 1 else date_part('year', age(now(),join_date)) end as work_year 
+                            from users r
+                            ) u on u.id = im.created_by and u.job_id = pc.jobs_id  and u.id = im.created_by  and u.work_year = pc.years 
+                        left join product_point pp on pp.product_id=ps.id and pp.branch_id=b.id 
+                        where pc.values > 0 and im.dated  between '".$begindate."' and '".$enddate."'   and c.branch_id::character varying like '%".$branchx."%' 
+                        group by u.id,b.remark,im.dated,u.work_year,u.name,im.invoice_no,ps.abbr,ps.remark,im.created_by,id.price,id.total,pc.values,id.qty 
                         union 
                         select  u.id,'referral' as com_type,im.dated,im.invoice_no,ps.abbr,ps.remark,im.created_by,u.name,id.price,id.qty,id.total,pc.referral_fee base_commision,pc.referral_fee  * id.qty as commisions  
                         from invoice_master im 
