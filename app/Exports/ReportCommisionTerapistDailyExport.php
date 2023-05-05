c8e0ee22b199c367df1ff5fed6bedfa9ed34c2e7
unknown <yogiektambakboyo@gmail.com>
2023-05-05 12:49:44 +0700

    fix export

--- app/Exports/ReportCommisionTerapistDailyExport.php
+++ app/Exports/ReportCommisionTerapistDailyExport.php
@@ -61,7 +61,7 @@ class ReportCommisionTerapistDailyExport implements FromCollection,WithColumnFor
                         ) u on u.id = id.assigned_to and u.job_id = pc.jobs_id  and u.id = id.assigned_to  and u.work_year = pc.years and u.id::character varying like '".$this->terapist."'
                     left join product_point pp on pp.product_id=ps.id and pp.branch_id=b.id 
                     where pc.values > 0 and im.dated between '".$this->begindate."' and '".$this->enddate."'  
-                    group by  b.remark,im.dated,u.work_year,u.name
+                    group by  u.work_year,b.remark,im.dated,u.work_year,u.name
                     union all            
                     select  b.remark as branch_name,'referral' as com_type,im.dated,count(ps.id) as qtyinv,case when u.work_year=0 then 1 when u.work_year>10 then 10  else u.work_year end as work_year,u.name,sum(pc.referral_fee * id.qty) as commisions,0 as point_qty   
                     from invoice_master im 
@@ -72,7 +72,7 @@ class ReportCommisionTerapistDailyExport implements FromCollection,WithColumnFor
                     join product_commisions pc on pc.product_id = id.product_id and pc.branch_id = c.branch_id
                     join users u on u.job_id = 2  and u.id = id.referral_by   and u.id::character varying like '".$this->terapist."'
                     where pc.referral_fee  > 0 and im.dated between '".$this->begindate."' and '".$this->enddate."'  
-                    group by  b.remark,im.dated,u.join_date,u.name
+                    group by  u.work_year,b.remark,im.dated,u.join_date,u.name
                     union all            
                     select b.remark as branch_name,'extra' as com_type,im.dated,count(ps.id) as qtyinv,case when u.work_year=0 then 1 when u.work_year>10 then 10  else u.work_year end as work_year,u.name,
                     sum(pc.assigned_to_fee * id.qty) commisions,
@@ -85,7 +85,7 @@ class ReportCommisionTerapistDailyExport implements FromCollection,WithColumnFor
                     join product_commisions pc on pc.product_id = id.product_id and pc.branch_id = c.branch_id
                     join users u on u.job_id = 2  and u.id = id.assigned_to  and u.id::character varying like '".$this->terapist."'
                     where pc.referral_fee+pc.assigned_to_fee+pc.created_by_fee  > 0  and im.dated  between '".$this->begindate."' and '".$this->enddate."'     and c.branch_id::character varying like  '%".$this->branch."%'
-                    group by  u.id,b.remark,im.dated,u.join_date,u.name
+                    group by  u.work_year,u.id,b.remark,im.dated,u.join_date,u.name
             ) a left join point_conversion pc2 on pc2.point_qty = a.point_qty  order by a.branch_name,a.dated,a.name
         ")); 
     }
