15d8f1fb47f9a9793b2d0818e2a51830ebddeb54
Yogi Aditya <yogiektambakboyo@gmail.com>
2023-10-07 20:58:28 +0700

    fix coout api

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -228,8 +228,6 @@ class LoginController extends Controller
         $notes = $request->service_selected;
         $customers_id = $request->customers_id;
         $item_list = $request->item_list;
-
-        return count($item_list);
         $data = DB::select(" INSERT INTO public.invoice_review
         (invoice_no, customers_id, value_review, remarks, created_at, notes)
         VALUES('".$invoice_no."', '".$customers_id."', '".$value_review."', '".$remarks."',now(), '".$notes."'); ");
