ff1e0f67ce49d777f9207fa5501b8e7e2e5001b4
unknown <yogiektambakboyo@gmail.com>
2023-11-08 11:44:46 +0700

    fix extra

--- resources/views/pages/reports/daily_print_2.blade.php
+++ resources/views/pages/reports/daily_print_2.blade.php
@@ -768,12 +768,12 @@
 
                         if(counter_extra.length>0){
                           letter_until = String.fromCharCode(letter_until.charCodeAt(0) - 1);
+                          worksheet.mergeCells(letter_until_start+(report_data.length+5), str_pref+letter_until+(report_data.length+5));
+                          worksheet.getCell(letter_until_start+(report_data.length+5)).value = ''+accounting.formatNumber(parseFloat(total_extra_rp/1000), 0, ".", ",")+'/'+accounting.formatNumber(parseFloat(total_extra_qty), 0, ".", ",");
+                          worksheet.getCell(letter_until_start+(report_data.length+5)).alignment = { vertical: 'middle', horizontal: 'center' };
+                          worksheet.getCell(letter_until_start+(report_data.length+5)).fill = {type: 'pattern',pattern:'solid',fgColor:{argb:'FFA726'}};
                         }
-                        worksheet.mergeCells(letter_until_start+(report_data.length+5), str_pref+letter_until+(report_data.length+5));
-                        worksheet.getCell(letter_until_start+(report_data.length+5)).value = ''+accounting.formatNumber(parseFloat(total_extra_rp/1000), 0, ".", ",")+'/'+accounting.formatNumber(parseFloat(total_extra_qty), 0, ".", ",");
-                        worksheet.getCell(letter_until_start+(report_data.length+5)).alignment = { vertical: 'middle', horizontal: 'center' };
-                        worksheet.getCell(letter_until_start+(report_data.length+5)).fill = {type: 'pattern',pattern:'solid',fgColor:{argb:'FFA726'}};
-
+                        
                         letter_until = String.fromCharCode(letter_until.charCodeAt(0) + 1);
                         var letter_until_start = letter_until;    
 
