772c25ba95594bf436a5e6fe920e110e28d2a150
Yogi Aditya Purnomo <yogiektambakboyo@gmail.com>
2024-02-02 19:34:28 +0700

    fix ojek

--- resources/views/pages/reports/close_day_sum_print_new.blade.php
+++ resources/views/pages/reports/close_day_sum_print_new.blade.php
@@ -401,7 +401,7 @@
       info: false,
       columnDefs: [
       { 
-        targets: [1,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23], 
+        targets: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23], 
         render: function ( data, type, row, meta ) {
           return type === 'export' ?
                     data.replaceAll('.','') :
