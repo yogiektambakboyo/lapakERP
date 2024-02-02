9391d064a50a9d9b263dba86a6517ecaed1fb996
Yogi Aditya Purnomo <yogiektambakboyo@gmail.com>
2024-02-02 19:57:55 +0700

    fix service

--- app/Http/Controllers/ReportCloseDayController.php
+++ app/Http/Controllers/ReportCloseDayController.php
@@ -1855,7 +1855,7 @@ class ReportCloseDayController extends Controller
         }else if($request->export=='Export Sum Counter'){
             $report_data = DB::select("
                     select b.id as branch_id,b.remark as branch_name,im.dated,sum(id.total+id.vat_total) as total_all,
-                    sum(case when ps.type_id = 2 and ps.category_id!=56 then id.total+id.vat_total else 0 end) as total_service,
+                    sum(case when ps.type_id = 2 then id.total+id.vat_total else 0 end) as total_service,
                     sum(case when ps.type_id = 1 and ps.category_id != 58 and ps.category_id != 60 then id.total+id.vat_total else 0 end) as total_product,
                     sum(case when ps.type_id = 1 and ps.category_id = 60 then id.total+id.vat_total else 0 end) as total_ojek,
                     sum(case when ps.type_id = 2 and ps.category_id=56 then id.qty*20000 else 0 end) as total_tambahan,
@@ -1890,7 +1890,7 @@ class ReportCloseDayController extends Controller
 
             $report_total = DB::select("
                     select 
-                    sum(case when ps.type_id = 2 and ps.category_id!=56  then id.total+id.vat_total else 0 end) as total_service,
+                    sum(case when ps.type_id = 2 then id.total+id.vat_total else 0 end) as total_service,
                     sum(case when ps.type_id = 2 and ps.category_id=53 then id.total+id.vat_total else 0 end) as total_salon,
                     sum(case when ps.type_id = 1 and ps.category_id != 60 then id.total+id.vat_total else 0 end) as total_product,
                     sum(case when ps.type_id = 1 and ps.category_id = 60 then id.total+id.vat_total else 0 end) as total_ojek,
