5af72cd01180c91569b70a272712f0c79e128ac3
yogiektambakboyo@gmail.com <yogiektambakboyo>
2024-04-26 12:36:50 +0700

    fix sum

--- app/Http/Controllers/ReportCashierComController.php
+++ app/Http/Controllers/ReportCashierComController.php
@@ -370,7 +370,7 @@ class ReportCashierComController extends Controller
                 string_agg(case when type_id=8 and a.abbr not like '%CAS LEBARAN%' then commisions::character varying else '' end,'##' order by right(invoice_no,6)) as commisions_extra,
                 string_agg(case when type_id=8 and a.abbr like '%CAS LEBARAN%' then commisions::character varying else '' end,'##' order by right(invoice_no,6)) as commisions_lebaran,
                 string_agg(case when type_id=2 then commisions::character varying else '' end,'##' order by right(invoice_no,6)) as charge_lebaran,
-                sum(a.commisions) as total   
+                sum(case when type_id=1 or (type_id=8 and a.abbr not like '%CAS LEBARAN%') then commisions else 0 end) as total   
                 from cashier_commision a 
                 join users_branch as ub on ub.branch_id = a.branch_id and ub.user_id = '".$user->id."'
                 where a.dated between '".$begindate."' and '".$enddate."'  and a.branch_id::character varying like '%".$branchx."%'
@@ -466,7 +466,7 @@ class ReportCashierComController extends Controller
                 string_agg(case when type_id=1 then qty::character varying else '' end,'##' order by right(invoice_no,6)) as product_qty,      
                 string_agg(case when type_id=1 then commisions::character varying else '' end,'##' order by right(invoice_no,6)) as product_commisions,   
                 string_agg(case when type_id=8 then commisions::character varying else '' end,'##' order by right(invoice_no,6)) as commisions_extra,
-                sum(a.commisions) as total   
+                sum(case when type_id=1 or (type_id=8 and a.abbr not like '%CAS LEBARAN%') then commisions else 0 end) as total   
                 from cashier_commision a 
                 join users_branch as ub on ub.branch_id = a.branch_id and ub.user_id = '".$user->id."'
                 where a.dated between '".$begindate."' and '".$enddate."'  and a.branch_id::character varying like '%".$branchx."%'
