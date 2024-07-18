90ed34b89dbd6179bb5750aa42d5c8b6a82b56cb
yogiektambakboyo@gmail.com <yogiektambakboyo>
2024-07-18 13:01:25 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -92,10 +92,14 @@ Route::group(['namespace' => 'App\Http\Controllers'], function()
     })->name('user.index'); **/
 
     Route::group(['middleware' => ['auth', 'permission']], function() {
+        Route::get('/kirimemail','NotifEmailController@send_mail');
+
         /**
          * Logout Routes
          */
         Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+
+
 
         /**
          * User Routes
